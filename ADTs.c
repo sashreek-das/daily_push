2b285649f20aa6630fd423d125d4dc4755d264c9
sashreek <21051167@kiit.ac.in>
2022-05-31 12:09:42 +0530

    first file

---
+++ ADTs.c
@@ -0,0 +1,51 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// typedef struct myArray{
+//     int total_size;
+//     int used_size;
+//     int *ptr;
+// }arr;
+// void createArray(arr *a,int tSize,int uSize){
+//     a->total_size=tSize;
+//     a->used_size=uSize;
+//     a->ptr=(int*)malloc(tSize*sizeof(int));
+// }
+// void setVal(arr *a){
+//     int n;
+//     for (int i = 0; i < a->used_size; i++)
+//     {
+//         printf("enter ");
+//         scanf("%d",&n);
+//         (a->ptr)[i]=n;
+//     }
+// }
+// void printArr(arr *a){
+//     for (int i = 0; i < a->used_size; i++)
+//     {
+//         printf("%d\n",(a->ptr)[i]);
+//     }
+    
+// }
+// int main(){
+//     arr marks;
+//     createArray(&marks,10,2);
+//     printf("\n");
+//     setVal(&marks);
+//     printf("\n");
+//     printArr(&marks);
+//     printf("\n");
+// return 0;
+// }
+// 
+#include <stdio.h>
+// struct employee{
+//     int code;
+//     char name[60];
+//     float salary;
+// };
+int main(){
+    // struct employee e1={121,"harry",122.0};
+    printf("j");
+
+return 0;
+}
